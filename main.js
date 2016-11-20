11c0f111f20f83c494b90e0f731adc84341e6243
Chris Poppell <clpoppell@valdosta.edu>
2016-11-20 15:34:54 -0500

    Placed all elements inside functions.

--- main.js
+++ main.js
@@ -1,54 +1,7 @@
+var player, scene, plane, camera, pointLight, renderer;
+currentlyPressedKeys= [];
 
-currentlyPressedKeys = [];
-
-var width = window.innerWidth;
-var height = window.innerHeight;
-
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
- 
-// create scene object
-var scene = new THREE.Scene;
-
-// create simple geometry and add to scene
-var cubeGeometry = new THREE.CubeGeometry(1,1, 1);
-
-//var cubeMaterial = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('crate.jpg')});
-var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000});
-var player = new THREE.Mesh(cubeGeometry, cubeMaterial); // Cube object is a stand in for the player model
-
-var sphereGeom = new THREE.SphereGeometry(1);
-var sphere = new THREE.Mesh(sphereGeom, cubeMaterial);
-sphere.position.set(2, 0, 1)
-
-var geometry = new THREE.PlaneGeometry( 5, 5, 32 );
-var material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
-var plane = new THREE.Mesh( geometry, material );
-plane.rotation.x = Math.PI / 2;
-// create perspective camera
-var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-camera.position.y = 5;
-camera.position.z = 1;
-camera.rotation.x = -Math.PI /2 + (1/4);
-
-// add to scene and renderer
-scene.add(camera); 
-renderer.render(scene, camera);
-
-// create the view matrix
-
-
-// add lighting and add to scene 
-var pointLight = new THREE.PointLight(0xaabbcc);
-pointLight.position.set(10, 16, 16);
-scene.add(pointLight);
-scene.add(player);
-scene.add(sphere);
-scene.add(plane)
-
-window.onload = function init()
-{
+window.onload= function init(){
     window.addEventListener("keydown", function(event) {
 		currentlyPressedKeys[event.keyCode] = true;
     });
@@ -56,52 +9,87 @@ window.onload = function init()
     window.addEventListener("keyup", function(event) {
 		currentlyPressedKeys[event.keyCode] = false;
     });
+	
+	var width= window.innerWidth;
+	var height= window.innerHeight;
 
+	renderer= new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
+	
+	// create scene object
+	scene= new THREE.Scene;
+
+	// create simple geometry and add to scene
+	var cubeGeometry = new THREE.CubeGeometry(1,1,1);
+
+	//var cubeMaterial = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('crate.jpg')});
+	var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000});
+	player = new THREE.Mesh(cubeGeometry, cubeMaterial); // Cube object is a stand in for the player model
+	player.position.set(0, 1, 49);
+
+	var sphereGeom = new THREE.SphereGeometry(1);
+	var sphere = new THREE.Mesh(sphereGeom, cubeMaterial);
+	sphere.position.set(3, 2, 5);
+
+	var geometry = new THREE.PlaneGeometry( 100, 100, 32 );
+	var material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+	plane = new THREE.Mesh( geometry, material );
+	plane.rotation.x = Math.PI / 2;
+	// create perspective camera
+	camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+	camera.position.x = player.position.x;
+	camera.position.y = 5;
+	camera.position.z = player.position.z+1;
+	camera.rotation.x = -Math.PI /2 + (1/4);
+
+	// add to scene and renderer
+	scene.add(camera); 
+	renderer.render(scene, camera);
+	
+	// add lighting and add to scene 
+	pointLight = new THREE.PointLight(0xaabbcc);
+	pointLight.position.set(10, 16, 16);
+	scene.add(pointLight);
+	scene.add(player);
+	scene.add(sphere);
+	scene.add(plane);
+
+	setupMaze();
     render();
 }
 
-/*
-var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
-var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
-var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-scene.add(skybox);
-
- 
-renderer.render(scene, camera);
-
-function handle_input()
-{
-    if(currentlyPressedKeys[65] == true) //A key
-    {
+function handle_input(){
+    if(currentlyPressedKeys[65] == true){ //A key
        camera.position.x -= .2;
        player.position.x -= .2;
     }
-    if(currentlyPressedKeys[68] == true) //D key
-    {
-        camera.position.x +=.2;
+    if(currentlyPressedKeys[68] == true){ //D key
+        camera.position.x += .2;
         player.position.x += .2;
     }
-    if(currentlyPressedKeys[87] == true) //W key
-    {
+    if(currentlyPressedKeys[87] == true){ //W key
         camera.position.z -= .2;
         player.position.z -= .2;
     }
-    if(currentlyPressedKeys[83] == true) //S key
-    {
-        camera.position.z +=.2;
+    if(currentlyPressedKeys[83] == true){ //S key
+        camera.position.z += .2;
         player.position.z += .2;
     }
-
-    //camera.lookAt(cube.position);
+	if(currentlyPressedKeys[81] == true){ camera.position.y += .5; }
+	if(currentlyPressedKeys[69] == true){ camera.position.y -= .5; }
 }
 
-function render() 
-{
-    
+function setupMaze(){
+	var wallGeometry= new THREE.CubeGeometry(0.5,10,10);
+	var wallMaterial= new THREE.MeshLambertMaterial({ color: 0x003399 });
+	var wall= new THREE.Mesh(wallGeometry, wallMaterial);
+	wall.position.set(2,1,45);
+	scene.add(wall);
+}
+
+function render(){
     handle_input();
     renderer.render(scene, camera);
     requestAnimationFrame(render);
-    player.rotation.y += 0.01;
-	
 }
-render();
